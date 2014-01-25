2a959b6902cf2c2f2aa8b6d02f05c7aca4e7787a
Shea <siarck@gmail.com>
2014-01-25 18:04:08 -0500

    made enemies attack

---
+++ Assets/Enemy.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed;
+    public int mapX = 0;
+    public int mapY = 0;
+    GameObject target = null;
+
+    void Start()
+    {
+        mapX = (int)(this.transform.position.x / GenerateRooms.roomGridWidthStatic);
+        mapY = (int)(this.transform.position.y / GenerateRooms.roomGridHeightStatic);
+    }
+
+    void Update()
+    {
+        if (target == null)
+        {
+            if (GenerateRooms.mapX == mapX && GenerateRooms.mapY == mapY)
+                target = GenerateRooms.player;
+        }
+        else
+        {
+            Vector3 dir = target.transform.position - this.transform.position;
+            this.transform.position += dir.normalized * speed * Time.deltaTime;
+        }
+    }
+}
