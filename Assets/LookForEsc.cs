1aaa7d6763921881bc752da6c09783d2aca6ad29
Shea <siarck@gmail.com>
2014-01-26 16:09:02 -0500

    added esc key

---
+++ Assets/LookForEsc.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookForEsc : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            Application.Quit();
+    }
+}
