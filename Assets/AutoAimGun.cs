372c1207f2f32c7b8f128947d96e0e17d9a23314
John Meister <john@supersoul.co>
2014-01-26 00:17:54 -0500

    added auto aim gun

---
+++ Assets/AutoAimGun.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AutoAimGun : MonoBehaviour 
+{
+	public float fireDelay = .25f;
+	private float timeElapsed = 0f;
+
+	public GameObject Bullet;
+	Vector3 target;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		timeElapsed += Time.deltaTime;
+
+		if (timeElapsed >= fireDelay)
+		{
+			Fire();
+			timeElapsed -= fireDelay;
+		}
+	}
+
+	void Fire()
+	{
+		GameObject[] activeEnemies = GameObject.FindGameObjectsWithTag("Enemy");
+		if (activeEnemies.Length < 1)
+		{
+			return;
+		}
+		target = activeEnemies[0].transform.position;
+		for (int i = 1; i < activeEnemies.Length; i++)
+		{
+			if ((activeEnemies[i].transform.position - this.gameObject.transform.position).magnitude < (target - this.gameObject.transform.position).magnitude)
+			{
+				target = activeEnemies[i].transform.position;
+			}                                                                                         
+		}
+
+		Vector3 dir = target - this.gameObject.transform.position;
+		if (Physics.Raycast(this.gameObject.transform.position, dir.normalized, dir.magnitude, 1<<LayerMask.NameToLayer("Wall")))
+		{
+			return;
+		}
+
+		GameObject temp = (GameObject)Instantiate(Bullet);
+		temp.transform.position = this.gameObject.transform.position;
+		temp.rigidbody.velocity = (target - this.gameObject.transform.position).normalized * 30f;
+		//fire at enemy
+
+	}
+}
